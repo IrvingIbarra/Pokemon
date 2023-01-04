b2b7c91c70895b09d0d04bfd33ede4b818e9946f
IrvingIbarra <irving_p.98@hotmail.com>
2023-01-04 13:46:59 -0600

    Obtencion de la informacion de los pokemones

--- js/pokeapi.js
+++ js/pokeapi.js
@@ -1,5 +1,6 @@
 export class Pokeapi{
     constructor(){
+        this.pokemones = null;
         let traerPokemones = this.obtenerPokemones().bind(this);
         traerPokemones();
 
@@ -8,17 +9,33 @@ export class Pokeapi{
     obtenerPokemones(){
         let f = async ()=>{
             const respuesta = await fetch('https://pokeapi.co/api/v2/pokemon/?limit=11&offset=10',{
-                headers: {
-                    'Content-Type' : 'application/json'
-                },
-                method: 'GET'
-                
-
+            method: 'GET'
             });
-            let resp = await respuesta.text();
-            console.log(resp);
+            if(respuesta.status === 200){
+                this.pokemones = await respuesta.json();
+                this.drawPokemones().bind(this)();
+            }
+            
         };
 
         return f;
     }
+
+    drawPokemones(){
+        let f =async ()=>{
+        if(this.pokemones){
+            let results = this.pokemones.results;//resilts es un array de object con dos atributos: name y url
+            for(let i =0; i < results.length; i++){
+                let pokemon = results[i];
+                const respuestaPokemon = await fetch(pokemon.url);
+                if(respuestaPokemon.status === 200){
+                    let infoPokemon = await respuestaPokemon.json();
+                    console.log(infoPokemon);
+                }
+            }
+       }
+    };
+    return f;
+    }
+
 }
